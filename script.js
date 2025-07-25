f17ee2b18e7a262574f14ab17a84574b9bfad658
krupal-05 <krupalchaudhari500@gmail.com>
2025-07-26 02:33:12 +0530

    add all data

---
+++ script.js
@@ -0,0 +1,55 @@
+const apid ="2db3336a6f00087bd6000a5f828e3a5e";
+const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+
+// search box 
+searchbtn.addEventListener("click", () => {
+    const city = searchbox.value;  
+    if (city.length == 0) {
+        alert("Please enter a city name");
+    }
+    else {
+        chackweather(city);
+    }
+});
+
+
+ 
+let icon = document.querySelector(".icon");
+
+
+async function chackweather(city) {
+    
+    const responce = await fetch(apiurl + city +`&appid=${apid}`);
+    var data = await responce.json();
+    console.log(data)
+
+document.querySelector(".city").innerHTML=data.name;
+document.querySelector(".temp").innerHTML=data.main.temp+" °C";
+document.querySelector(".humi").innerHTML= data.main.humidity+" %";
+document.querySelector(".speed").innerHTML= data.wind.speed+" km/h";
+document.querySelector(".adata").style.display="flex";
+
+if(data.weather[0].main == "Clouds"){
+        icon.src="/wether chack/img/clouds.png"
+}
+else if(data.weather[0].main=="clear"){
+        icon.src="/wether chack/img/clear.png"
+}
+else if(data.weather[0].main=="drizzle"){
+        icon.src="/wether chack/img/drizzle.png"
+}
+else if(data.weather[0].main=="mist"){
+        icon.src="/wether chack/img/mist.png"
+}
+else if(data.weather[0].main=="rain"){
+        icon.src="/wether chack/img/rain.png"
+}
+else if(data.weather[0].main=="snow"){
+        icon.src="/wether chack/img/snow.png"
+}
+
+
+}
