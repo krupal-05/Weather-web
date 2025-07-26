fcbe9468d3436ac6a59d9f5588de64b13ec7bdd8
krupal-05 <krupalchaudhari500@gmail.com>
2025-07-26 23:00:38 +0530

    modifiaon

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const apid ="2db3336a6f00087bd6000a5f828e3a5e";
-const apiurl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const apiurl ="https:/api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 const searchbox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
 
-// search box 
+/ search box 
 searchbtn.addEventListener("click", () => {
     const city = searchbox.value;  
     if (city.length == 0) {
@@ -33,22 +33,22 @@ document.querySelector(".speed").innerHTML= data.wind.speed+" km/h";
 document.querySelector(".adata").style.display="flex";
 
 if(data.weather[0].main == "Clouds"){
-        icon.src="/Weather-web//img/clouds.png"
+        icon.src="/Weather-web/img/clouds.png"
 }
 else if(data.weather[0].main=="clear"){
-        icon.src="/Weather-web//img/clear.png"
+        icon.src="/Weather-web/img/clear.png"
 }
 else if(data.weather[0].main=="drizzle"){
-        icon.src="/Weather-web//img/drizzle.png"
+        icon.src="/Weather-web/img/drizzle.png"
 }
 else if(data.weather[0].main=="mist"){
-        icon.src="/Weather-web//img/mist.png"
+        icon.src="/Weather-web/img/mist.png"
 }
 else if(data.weather[0].main=="rain"){
-        icon.src="/Weather-web//img/rain.png"
+        icon.src="/Weather-web/img/rain.png"
 }
 else if(data.weather[0].main=="snow"){
-        icon.src="/Weather-web//img/snow.png"
+        icon.src="/Weather-web/img/snow.png"
 }
 
 
